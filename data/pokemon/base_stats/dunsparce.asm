a1b5d157b403f344a39d94937931f3debcaa89aa
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-08-25 13:58:22 +0100

    Update dunsparce.asm

--- data/pokemon/base_stats/dunsparce.asm
+++ data/pokemon/base_stats/dunsparce.asm
@@ -1,9 +1,10 @@
 	db DUNSPARCE ; 206
 
-	db 100,  70,  70,  45,  65,  65
+	; db 100,  70,  70,  45,  65,  65
+	  db 90, 90, 75, 55, 30, 70
 	;   hp  atk  def  spd  sat  sdf
 
-	db NORMAL, NORMAL ; type
+	db NORMAL, GROUND; NORMAL ; type
 	db 190 ; catch rate
 	db 75 ; base exp
 	db NO_ITEM, NO_ITEM ; items
