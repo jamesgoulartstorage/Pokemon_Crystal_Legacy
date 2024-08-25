aaca370ed31b5f37c54c340b9dbcf1ec828f4725
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-08-25 19:28:28 +0100

    Update tyranitar.asm

--- data/pokemon/base_stats/tyranitar.asm
+++ data/pokemon/base_stats/tyranitar.asm
@@ -1,6 +1,7 @@
 	db TYRANITAR ; 248
 
-	db 100, 134, 110,  61,  95, 100
+	; db 100, 134, 110,  61,  95, 100
+	  db 110, 134, 110, 81, 55, 110
 	;   hp  atk  def  spd  sat  sdf
 
 	db ROCK, DARK ; type
