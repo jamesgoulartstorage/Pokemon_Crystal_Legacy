80aa2b77732f79a4314d5bbb8f0894baab5bf8bd
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-08-25 13:23:25 +0100

    Update arcanine.asm

--- data/pokemon/base_stats/arcanine.asm
+++ data/pokemon/base_stats/arcanine.asm
@@ -1,9 +1,10 @@
 	db ARCANINE ; 059
 
-	db  90, 110,  80,  95, 100,  80
+	; db  90, 110,  80,  95, 100,  80
+	db 90, 70, 80, 115, 120, 80
 	;   hp  atk  def  spd  sat  sdf
 
-	db FIRE, FIRE ; type
+	db FIRE, PSYCHIC; FIRE ; type
 	db 75 ; catch rate
 	db 213 ; base exp
 	db BURNT_BERRY, BURNT_BERRY ; items
