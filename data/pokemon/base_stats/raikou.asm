64816103936fda2eb590c1ddbfc46b14b1861ec8
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-08-25 15:38:09 +0100

    Update raikou.asm

--- data/pokemon/base_stats/raikou.asm
+++ data/pokemon/base_stats/raikou.asm
@@ -1,6 +1,7 @@
 	db RAIKOU ; 243
 
-	db  90,  85,  75, 115, 115, 100
+	; db  90,  85,  75, 115, 115, 100
+	  db 90, 70, 75, 125, 120, 100
 	;   hp  atk  def  spd  sat  sdf
 
 	db ELECTRIC, ELECTRIC ; type
