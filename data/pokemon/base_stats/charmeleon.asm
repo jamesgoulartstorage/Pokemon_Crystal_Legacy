fea72be165bce914f50b1c8f49c5f2d6228f69df
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-09 03:46:12 +0100

    Update charmeleon.asm

--- data/pokemon/base_stats/charmeleon.asm
+++ data/pokemon/base_stats/charmeleon.asm
@@ -6,7 +6,8 @@
 	db FIRE, FIRE ; type
 	db 45 ; catch rate
 	db 142 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db BURNT_BERRY, CHARCOAL;
 	db GENDER_F12_5 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
