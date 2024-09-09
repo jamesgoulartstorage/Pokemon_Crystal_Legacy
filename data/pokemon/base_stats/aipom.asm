359e7f402ece37000f6b1c697733fa713835a0cc
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-09 03:36:31 +0100

    Update aipom.asm

--- data/pokemon/base_stats/aipom.asm
+++ data/pokemon/base_stats/aipom.asm
@@ -7,7 +7,8 @@
 	db NORMAL, GRASS; NORMAL ; type
 	db 45 ; catch rate
 	db 94 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db BLU_APRICORN, SUN_STONE;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
