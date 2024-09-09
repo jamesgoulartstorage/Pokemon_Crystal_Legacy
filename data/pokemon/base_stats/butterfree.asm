cb053af8c8116ec21170586b922d8cb37273fec6
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-09 03:43:10 +0100

    Update butterfree.asm

--- data/pokemon/base_stats/butterfree.asm
+++ data/pokemon/base_stats/butterfree.asm
@@ -6,7 +6,8 @@
 	db BUG, FLYING ; type
 	db 45 ; catch rate
 	db 160 ; base exp
-	db NO_ITEM, SILVERPOWDER ; items
+	; db NO_ITEM, SILVERPOWDER ; items
+	db PRZCUREBERRY, SILVERPOWDER;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 4 ; step cycles to hatch
