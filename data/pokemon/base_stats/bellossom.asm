0f5fa970770ef0cbe010d6907d26604c5bebb69d
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-09 03:41:19 +0100

    Update bellossom.asm

--- data/pokemon/base_stats/bellossom.asm
+++ data/pokemon/base_stats/bellossom.asm
@@ -7,7 +7,8 @@
 	db GRASS, PSYCHIC_TYPE; GRASS ; type
 	db 45 ; catch rate
 	db 184 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db MIRACLEBERRY, MIRACLE_SEED;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
