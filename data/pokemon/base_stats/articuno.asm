06b6a473c9d68c5fc81bd24e9e00e713d23bb231
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-09 03:39:23 +0100

    Update articuno.asm

--- data/pokemon/base_stats/articuno.asm
+++ data/pokemon/base_stats/articuno.asm
@@ -6,7 +6,8 @@
 	db ICE, FLYING ; type
 	db 3 ; catch rate
 	db 215 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db NEVERMELTICE, LUCKY_EGG;
 	db GENDER_UNKNOWN ; gender ratio
 	db 100 ; unknown 1
 	db 20 ; step cycles to hatch
