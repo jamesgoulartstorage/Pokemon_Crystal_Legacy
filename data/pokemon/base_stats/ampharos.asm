0c132bfd84980368e1cbe08c68ea2af8835d9f31
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-09 03:37:33 +0100

    Update ampharos.asm

--- data/pokemon/base_stats/ampharos.asm
+++ data/pokemon/base_stats/ampharos.asm
@@ -6,7 +6,8 @@
 	db ELECTRIC, DRAGON; ELECTRIC ; type
 	db 45 ; catch rate
 	db 194 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db LIGHT_BALL, DRAGON_SCALE;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
