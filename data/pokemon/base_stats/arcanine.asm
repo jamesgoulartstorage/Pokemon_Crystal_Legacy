3b8ae6dd23d97b1693bac6e5b816e18c46d7633f
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-09 03:38:26 +0100

    Update arcanine.asm

--- data/pokemon/base_stats/arcanine.asm
+++ data/pokemon/base_stats/arcanine.asm
@@ -7,7 +7,8 @@
 	db FIRE, PSYCHIC_TYPE; FIRE ; type
 	db 75 ; catch rate
 	db 213 ; base exp
-	db BURNT_BERRY, BURNT_BERRY ; items
+	; db BURNT_BERRY, BURNT_BERRY ; items
+	db CHARCOAL, QUICK_CLAW;
 	db GENDER_F25 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
