b68b6dc43ad2ef30f1173ba081d4b8932293db39
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-09 03:35:58 +0100

    Update aerodactyl.asm

--- data/pokemon/base_stats/aerodactyl.asm
+++ data/pokemon/base_stats/aerodactyl.asm
@@ -7,7 +7,8 @@
 	db ROCK, FLYING ; type
 	db 45 ; catch rate
 	db 202 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db HARD_STONE, DRAGON_FANG;
 	db GENDER_F12_5 ; gender ratio
 	db 100 ; unknown 1
 	db 9 ; step cycles to hatch
