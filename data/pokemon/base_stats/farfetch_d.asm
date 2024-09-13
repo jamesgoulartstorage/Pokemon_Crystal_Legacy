10166f7db139ab3e2d158f24fa3264f2657c6a64
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 00:36:51 +0100

    Update farfetch_d.asm

--- data/pokemon/base_stats/farfetch_d.asm
+++ data/pokemon/base_stats/farfetch_d.asm
@@ -7,7 +7,8 @@
 	db FLYING, FIGHTING; NORMAL, FLYING ; type
 	db 45 ; catch rate
 	db 94 ; base exp
-	db NO_ITEM, STICK ; items
+	; db NO_ITEM, STICK ; items
+	db STICK, STICK;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
