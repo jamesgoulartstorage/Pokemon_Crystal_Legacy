4e618bf29f2e5cf8cde9d46f3d0fa4f60afaa59b
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 00:20:55 +0100

    Update delibird.asm

--- data/pokemon/base_stats/delibird.asm
+++ data/pokemon/base_stats/delibird.asm
@@ -6,7 +6,8 @@
 	db ICE, FLYING ; type
 	db 45 ; catch rate
 	db 183 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db ICE_BERRY, NEVERMELTICE;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
