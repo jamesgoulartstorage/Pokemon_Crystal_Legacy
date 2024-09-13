62c2e2686a9de04c6981e5b7d5b0f0e3a77fe67c
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 00:45:00 +0100

    Update gligar.asm

--- data/pokemon/base_stats/gligar.asm
+++ data/pokemon/base_stats/gligar.asm
@@ -7,7 +7,8 @@
 	db GROUND, FLYING ; type
 	db 60 ; catch rate
 	db 108 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db PSNCUREBERRY, POISON_BARB;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
