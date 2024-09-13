586d613862eb82c18fbd037d22e9dd2eb52acd1d
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 00:18:57 +0100

    Update corsola.asm

--- data/pokemon/base_stats/corsola.asm
+++ data/pokemon/base_stats/corsola.asm
@@ -6,7 +6,8 @@
 	db WATER, ROCK ; type
 	db 60 ; catch rate
 	db 113 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db WATER_STONE, STAR_PIECE;
 	db GENDER_F75 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
