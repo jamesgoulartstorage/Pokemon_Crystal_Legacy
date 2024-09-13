e0f0ebcc6e5f9ee2e4d765623a0832e09a1ed5d9
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 00:43:31 +0100

    Update gengar.asm

--- data/pokemon/base_stats/gengar.asm
+++ data/pokemon/base_stats/gengar.asm
@@ -7,7 +7,8 @@
 	db GHOST, ICE; POISON ; type
 	db 45 ; catch rate
 	db 190 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db SMOKE_BALL, SPELL_TAG;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
