364397adbbbafbdb0038c682182047a8b5f8627d
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 00:41:23 +0100

    Update flareon.asm

--- data/pokemon/base_stats/flareon.asm
+++ data/pokemon/base_stats/flareon.asm
@@ -7,7 +7,8 @@
 	db FIRE, FIRE ; type
 	db 45 ; catch rate
 	db 198 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db RED_APRICORN, CHARCOAL;
 	db GENDER_F12_5 ; gender ratio
 	db 100 ; unknown 1
 	db 9 ; step cycles to hatch
