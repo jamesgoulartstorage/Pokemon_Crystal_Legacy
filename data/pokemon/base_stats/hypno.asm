fa00f8fa866421bf5aa1eeafd22c352a9d9c4f43
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 00:56:00 +0100

    Update hypno.asm

--- data/pokemon/base_stats/hypno.asm
+++ data/pokemon/base_stats/hypno.asm
@@ -7,7 +7,8 @@
 	db PSYCHIC_TYPE, GHOST; PSYCHIC_TYPE ; type
 	db 75 ; catch rate
 	db 165 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db SPELL_TAG, TWISTEDSPOON;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
