cdfd74a8d0327767b0dfc1c331cad30cf12e99cc
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 00:30:20 +0100

    Update ekans.asm

--- data/pokemon/base_stats/ekans.asm
+++ data/pokemon/base_stats/ekans.asm
@@ -7,7 +7,8 @@
 	db POISON, POISON ; type
 	db 255 ; catch rate
 	db 62 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db ANTIDOTE, POISON_BARB;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
