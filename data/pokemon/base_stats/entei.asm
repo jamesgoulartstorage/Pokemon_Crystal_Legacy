25d7e0fe3cc173082b1376f269ba2f9835398724
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 00:34:48 +0100

    Update entei.asm

--- data/pokemon/base_stats/entei.asm
+++ data/pokemon/base_stats/entei.asm
@@ -7,7 +7,8 @@
 	db FIRE, GHOST; FIRE ; type
 	db 3 ; catch rate
 	db 217 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db CHARCOAL, SACRED_ASH;
 	db GENDER_UNKNOWN ; gender ratio
 	db 100 ; unknown 1
 	db 20 ; step cycles to hatch
