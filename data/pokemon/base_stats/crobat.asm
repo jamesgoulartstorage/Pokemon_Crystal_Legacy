dd073e8f93bbb74546228ab9b5e29bf2d594b3ea
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 00:19:21 +0100

    Update crobat.asm

--- data/pokemon/base_stats/crobat.asm
+++ data/pokemon/base_stats/crobat.asm
@@ -7,7 +7,8 @@
 	db POISON, FLYING ; type
 	db 90 ; catch rate
 	db 204 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db PSNCUREBERRY, SCOPE_LENS;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 4 ; step cycles to hatch
