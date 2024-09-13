4fdb13bd1fd9ed74b6316ce743b9a8f7bf18682e
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 00:23:34 +0100

    Update doduo.asm

--- data/pokemon/base_stats/doduo.asm
+++ data/pokemon/base_stats/doduo.asm
@@ -7,7 +7,8 @@
 	db NORMAL, FLYING ; type
 	db 190 ; catch rate
 	db 96 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db BITTER_BERRY, SHARP_BEAK;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
