8db23db711850bfb7cd3c19c87d1889db91a4fac
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 00:48:55 +0100

    Update grimer.asm

--- data/pokemon/base_stats/grimer.asm
+++ data/pokemon/base_stats/grimer.asm
@@ -6,7 +6,8 @@
 	db POISON, POISON ; type
 	db 190 ; catch rate
 	db 90 ; base exp
-	db NO_ITEM, NUGGET ; items
+	; db NO_ITEM, NUGGET ; items
+	db POISON_BARB, NUGGET;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
