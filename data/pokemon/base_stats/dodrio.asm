0fa2cdceba5eda49580126f1776273fbe0b77a88
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 00:23:07 +0100

    Update dodrio.asm

--- data/pokemon/base_stats/dodrio.asm
+++ data/pokemon/base_stats/dodrio.asm
@@ -7,7 +7,8 @@
 	db FIGHTING, FLYING; NORMAL, FLYING ; type
 	db 45 ; catch rate
 	db 158 ; base exp
-	db NO_ITEM, SHARP_BEAK ; items
+	; db NO_ITEM, SHARP_BEAK ; items
+	db SHARP_BEAK, MYSTERYBERRY;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
