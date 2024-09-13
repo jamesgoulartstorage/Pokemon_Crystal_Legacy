9775faba43cf621633ad1a3fb1571ff1080c24c4
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 00:55:23 +0100

    Update houndoom.asm

--- data/pokemon/base_stats/houndoom.asm
+++ data/pokemon/base_stats/houndoom.asm
@@ -7,7 +7,8 @@
 	db DARK, FIRE ; type
 	db 45 ; catch rate
 	db 204 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db BURNT_BERRY, CHARCOAL;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
