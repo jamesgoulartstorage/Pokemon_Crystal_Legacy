9d54a1b2c6dc7c3e4af9c71e9674c30203feefa3
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 00:43:11 +0100

    Update gastly.asm

--- data/pokemon/base_stats/gastly.asm
+++ data/pokemon/base_stats/gastly.asm
@@ -6,7 +6,8 @@
 	db GHOST, POISON ; type
 	db 190 ; catch rate
 	db 95 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db SMOKE_BALL, SPELL_TAG;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
