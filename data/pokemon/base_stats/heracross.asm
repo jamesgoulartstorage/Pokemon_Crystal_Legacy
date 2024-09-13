3241f4b7c2e1915565783adc8d7a889a968e828b
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 00:51:42 +0100

    Update heracross.asm

--- data/pokemon/base_stats/heracross.asm
+++ data/pokemon/base_stats/heracross.asm
@@ -7,7 +7,8 @@
 	db BUG, FIGHTING ; type
 	db 45 ; catch rate
 	db 200 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db PRZCUREBERRY, SILVERPOWDER;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 6 ; step cycles to hatch
