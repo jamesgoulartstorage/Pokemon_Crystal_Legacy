2facc74216cbeed3f51c3c439ad6fff31223c4a6
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 00:35:11 +0100

    Update espeon.asm

--- data/pokemon/base_stats/espeon.asm
+++ data/pokemon/base_stats/espeon.asm
@@ -7,7 +7,8 @@
 	db PSYCHIC_TYPE, PSYCHIC_TYPE ; type
 	db 45 ; catch rate
 	db 197 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db WHT_APRICORN, TWISTEDSPOON;
 	db GENDER_F12_5 ; gender ratio
 	db 100 ; unknown 1
 	db 9 ; step cycles to hatch
