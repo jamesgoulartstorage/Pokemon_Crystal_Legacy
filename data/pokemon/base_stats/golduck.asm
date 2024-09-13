792c08024e73a6b874e059bc5f67be929e386502
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 00:47:21 +0100

    Update golduck.asm

--- data/pokemon/base_stats/golduck.asm
+++ data/pokemon/base_stats/golduck.asm
@@ -7,7 +7,8 @@
 	db WATER, PSYCHIC_TYPE; WATER ; type
 	db 75 ; catch rate
 	db 174 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db FRESH_WATER, MYSTIC_WATER;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
