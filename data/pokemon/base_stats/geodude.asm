e1a35a71aedaaec9eff12653c666d6ee570fd57c
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 00:44:02 +0100

    Update geodude.asm

--- data/pokemon/base_stats/geodude.asm
+++ data/pokemon/base_stats/geodude.asm
@@ -6,7 +6,8 @@
 	db ROCK, GROUND ; type
 	db 255 ; catch rate
 	db 86 ; base exp
-	db NO_ITEM, EVERSTONE ; items
+	; db NO_ITEM, EVERSTONE ; items
+	db HARD_STONE, THUNDERSTONE;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 4 ; step cycles to hatch
