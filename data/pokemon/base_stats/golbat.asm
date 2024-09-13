cf6b74ff1bbd56adffa1b3755dbf07a8b45d8b4e
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 00:45:53 +0100

    Update golbat.asm

--- data/pokemon/base_stats/golbat.asm
+++ data/pokemon/base_stats/golbat.asm
@@ -6,7 +6,8 @@
 	db POISON, FLYING ; type
 	db 90 ; catch rate
 	db 171 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db RED_APRICORN, BLK_APRICORN;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 4 ; step cycles to hatch
