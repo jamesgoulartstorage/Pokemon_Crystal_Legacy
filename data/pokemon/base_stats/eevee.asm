933b548079b96fdde4cc8768e1224ffa4f8181cc
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 00:29:58 +0100

    Update eevee.asm

--- data/pokemon/base_stats/eevee.asm
+++ data/pokemon/base_stats/eevee.asm
@@ -6,7 +6,8 @@
 	db NORMAL, NORMAL ; type
 	db 45 ; catch rate
 	db 92 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db POLKADOT_BOW, EVERSTONE;
 	db GENDER_F12_5 ; gender ratio
 	db 100 ; unknown 1
 	db 9 ; step cycles to hatch
