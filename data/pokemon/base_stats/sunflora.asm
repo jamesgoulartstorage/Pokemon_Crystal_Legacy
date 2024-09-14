c1b7e748e0c3d80a9e4abb586026fc8dbc806fc5
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 02:28:00 +0100

    Update sunflora.asm

--- data/pokemon/base_stats/sunflora.asm
+++ data/pokemon/base_stats/sunflora.asm
@@ -7,7 +7,8 @@
 	db GRASS, FIRE; GRASS ; type
 	db 120 ; catch rate
 	db 146 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db BURNT_BERRY, SUN_STONE;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
