77e375d1d7576a0743c93d36c815f8d0af3e61f3
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 02:01:15 +0100

    Update phanpy.asm

--- data/pokemon/base_stats/phanpy.asm
+++ data/pokemon/base_stats/phanpy.asm
@@ -7,7 +7,8 @@
 	db GROUND, NORMAL; GROUND ; type
 	db 120 ; catch rate
 	db 124 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db MINT_BERRY, SOFT_SAND;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
