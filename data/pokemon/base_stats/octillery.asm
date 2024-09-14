07eb4f87391683f7190cf58ab5c7b8ef1f7545a9
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 01:59:08 +0100

    Update octillery.asm

--- data/pokemon/base_stats/octillery.asm
+++ data/pokemon/base_stats/octillery.asm
@@ -7,7 +7,8 @@
 	db WATER, FIRE; WATER ; type
 	db 75 ; catch rate
 	db 164 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db FRESH_WATER, SMOKE_BALL;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
