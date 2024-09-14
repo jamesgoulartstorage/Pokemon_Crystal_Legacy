e95122cd815a986f63381653e63d1259acb5926f
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 02:39:11 +0100

    Update wartortle.asm

--- data/pokemon/base_stats/wartortle.asm
+++ data/pokemon/base_stats/wartortle.asm
@@ -6,7 +6,8 @@
 	db WATER, WATER ; type
 	db 45 ; catch rate
 	db 143 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db FRESH_WATER, STARDUST;
 	db GENDER_F12_5 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
