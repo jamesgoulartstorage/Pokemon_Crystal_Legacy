2e0e5a617ec065ef85b50d542081b7ba41203ea8
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 02:19:38 +0100

    Update slowking.asm

--- data/pokemon/base_stats/slowking.asm
+++ data/pokemon/base_stats/slowking.asm
@@ -7,7 +7,8 @@
 	db WATER, PSYCHIC_TYPE ; type
 	db 70 ; catch rate
 	db 164 ; base exp
-	db NO_ITEM, KINGS_ROCK ; items
+	; db NO_ITEM, KINGS_ROCK ; items
+	db MYSTIC_WATER, KINGS_ROCK;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
