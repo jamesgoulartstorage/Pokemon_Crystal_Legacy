fc3bef389c7c2768c726543a6d98b3dbe9f81950
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 02:25:57 +0100

    Update stantler.asm

--- data/pokemon/base_stats/stantler.asm
+++ data/pokemon/base_stats/stantler.asm
@@ -6,7 +6,8 @@
 	db NORMAL, PSYCHIC_TYPE; NORMAL ; type
 	db 45 ; catch rate
 	db 165 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db BITTER_BERRY, MYSTERYBERRY;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
