f161c82251f8e1822bd90e18a8a6080d8a57cf63
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 02:24:21 +0100

    Update snubbull.asm

--- data/pokemon/base_stats/snubbull.asm
+++ data/pokemon/base_stats/snubbull.asm
@@ -7,7 +7,8 @@
 	db NORMAL, NORMAL ; type
 	db 190 ; catch rate
 	db 63 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db PNK_APRICORN, POLKADOT_BOW;
 	db GENDER_F75 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
