02cb709278ba867fdca134d529c5dd752119396c
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 01:51:44 +0100

    Update metapod.asm

--- data/pokemon/base_stats/metapod.asm
+++ data/pokemon/base_stats/metapod.asm
@@ -6,7 +6,8 @@
 	db BUG, BUG ; type
 	db 120 ; catch rate
 	db 72 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db PRZCUREBERRY, SILVERPOWDER;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 4 ; step cycles to hatch
