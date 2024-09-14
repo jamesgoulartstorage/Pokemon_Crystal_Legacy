0f0b08bb57e99dec74eb993f67f311029aea9aa0
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 02:26:17 +0100

    Update starmie.asm

--- data/pokemon/base_stats/starmie.asm
+++ data/pokemon/base_stats/starmie.asm
@@ -7,7 +7,8 @@
 	db WATER, PSYCHIC_TYPE ; type
 	db 60 ; catch rate
 	db 207 ; base exp
-	db STARDUST, STAR_PIECE ; items
+	; db STARDUST, STAR_PIECE ; items
+	db STAR_PIECE, WATER_STONE;
 	db GENDER_UNKNOWN ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
