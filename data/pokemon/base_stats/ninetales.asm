cff62a4adb63dcd047fb568641ec5f9989f3558a
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 01:58:18 +0100

    Update ninetales.asm

--- data/pokemon/base_stats/ninetales.asm
+++ data/pokemon/base_stats/ninetales.asm
@@ -7,7 +7,8 @@
 	db FIRE, GHOST; FIRE ; type
 	db 75 ; catch rate
 	db 178 ; base exp
-	db BURNT_BERRY, BURNT_BERRY ; items
+	; db BURNT_BERRY, BURNT_BERRY ; items
+	db CHARCOAL, SPELL_TAG;
 	db GENDER_F75 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
