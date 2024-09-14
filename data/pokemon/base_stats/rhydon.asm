2832c5e37377594f777d8620bc0a24d7764d6169
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 02:14:25 +0100

    Update rhydon.asm

--- data/pokemon/base_stats/rhydon.asm
+++ data/pokemon/base_stats/rhydon.asm
@@ -7,7 +7,8 @@
 	db GROUND, ROCK ; type
 	db 60 ; catch rate
 	db 204 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db BLK_APRICORN, HARD_STONE;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
