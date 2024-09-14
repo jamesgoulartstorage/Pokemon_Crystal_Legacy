b04d766510b10e0f065a0fd58d2ed57f06f718ae
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 02:20:03 +0100

    Update slowpoke.asm

--- data/pokemon/base_stats/slowpoke.asm
+++ data/pokemon/base_stats/slowpoke.asm
@@ -6,7 +6,8 @@
 	db WATER, PSYCHIC_TYPE ; type
 	db 190 ; catch rate
 	db 99 ; base exp
-	db NO_ITEM, KINGS_ROCK ; items
+	; db NO_ITEM, KINGS_ROCK ; items
+	db SLOWPOKETAIL, SUPER_ROD;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
