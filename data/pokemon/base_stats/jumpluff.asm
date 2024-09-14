cfe9708affe95cd7e3d81ca70cc2b4572b7142f8
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 01:37:36 +0100

    Update jumpluff.asm

--- data/pokemon/base_stats/jumpluff.asm
+++ data/pokemon/base_stats/jumpluff.asm
@@ -6,7 +6,8 @@
 	db GRASS, FLYING ; type
 	db 45 ; catch rate
 	db 176 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db BLU_APRICORN, MIRACLE_SEED;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
