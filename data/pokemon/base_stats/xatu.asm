b7d850c84cef79d6bdc2187337cf863fd1a41ee6
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 02:41:51 +0100

    Update xatu.asm

--- data/pokemon/base_stats/xatu.asm
+++ data/pokemon/base_stats/xatu.asm
@@ -6,7 +6,8 @@
 	db PSYCHIC_TYPE, FLYING ; type
 	db 75 ; catch rate
 	db 171 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db SHARP_BEAK, TWISTEDSPOON;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
