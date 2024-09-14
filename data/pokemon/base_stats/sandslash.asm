72c4c0d8dc1b80d9703f0cb8b41e6bc509c42da2
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 02:15:30 +0100

    Update sandslash.asm

--- data/pokemon/base_stats/sandslash.asm
+++ data/pokemon/base_stats/sandslash.asm
@@ -7,7 +7,8 @@
 	db GROUND, STEEL; GROUND ; type
 	db 90 ; catch rate
 	db 163 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db SOFT_SAND, METAL_COAT;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
