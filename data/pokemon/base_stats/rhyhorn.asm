460b8788f78808435ec2ae43aede7b3852beb206
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 02:14:53 +0100

    Update rhyhorn.asm

--- data/pokemon/base_stats/rhyhorn.asm
+++ data/pokemon/base_stats/rhyhorn.asm
@@ -6,7 +6,8 @@
 	db GROUND, ROCK ; type
 	db 120 ; catch rate
 	db 135 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db BLK_APRICORN, HARD_STONE;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
