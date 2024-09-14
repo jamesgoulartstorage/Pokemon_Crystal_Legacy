45b5f637fd30c8b1aa17a22ea8ba958445a71b7e
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 02:17:06 +0100

    Update seaking.asm

--- data/pokemon/base_stats/seaking.asm
+++ data/pokemon/base_stats/seaking.asm
@@ -7,7 +7,8 @@
 	db WATER, WATER ; type
 	db 60 ; catch rate
 	db 170 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db FRESH_WATER, POISON_BARB;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
