b900b6100d88fa7d1a45826b22e4f27570e4d311
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 02:41:31 +0100

    Update wooper.asm

--- data/pokemon/base_stats/wooper.asm
+++ data/pokemon/base_stats/wooper.asm
@@ -7,7 +7,8 @@
 	db WATER, GROUND ; type
 	db 255 ; catch rate
 	db 52 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db FRESH_WATER, SOFT_SAND;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
