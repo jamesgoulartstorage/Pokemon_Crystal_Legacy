661c71a20dd65ec6341dc0c4777c1a95188ac19d
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 02:15:50 +0100

    Update scizor.asm

--- data/pokemon/base_stats/scizor.asm
+++ data/pokemon/base_stats/scizor.asm
@@ -7,7 +7,8 @@
 	db BUG, STEEL ; type
 	db 25 ; catch rate
 	db 200 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db SILVERPOWDER, METAL_POWDER;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 6 ; step cycles to hatch
