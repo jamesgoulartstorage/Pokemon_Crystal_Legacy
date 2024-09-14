c7631829345c062a3211404bb78a1bbf28c1ac48
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 01:59:44 +0100

    Update omanyte.asm

--- data/pokemon/base_stats/omanyte.asm
+++ data/pokemon/base_stats/omanyte.asm
@@ -7,7 +7,8 @@
 	db ROCK, WATER ; type
 	db 45 ; catch rate
 	db 120 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db MYSTIC_WATER, WATER_STONE;
 	db GENDER_F12_5 ; gender ratio
 	db 100 ; unknown 1
 	db 8 ; step cycles to hatch
