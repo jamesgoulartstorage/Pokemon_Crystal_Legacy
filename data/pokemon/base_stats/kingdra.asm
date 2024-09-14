268ac91739f2519e0544eaa5f55731b01ccb5015
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 01:40:41 +0100

    Update kingdra.asm

--- data/pokemon/base_stats/kingdra.asm
+++ data/pokemon/base_stats/kingdra.asm
@@ -7,7 +7,8 @@
 	db WATER, DRAGON ; type
 	db 45 ; catch rate
 	db 207 ; base exp
-	db NO_ITEM, DRAGON_SCALE ; items
+	; db NO_ITEM, DRAGON_SCALE ; items
+	db MYSTIC_WATER, DRAGON_SCALE;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
