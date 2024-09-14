a1ce1a715bd4ad58502905a0edbe809e91826ff8
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 01:45:31 +0100

    Update machoke.asm

--- data/pokemon/base_stats/machoke.asm
+++ data/pokemon/base_stats/machoke.asm
@@ -7,7 +7,8 @@
 	db FIGHTING, FIGHTING ; type
 	db 90 ; catch rate
 	db 146 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db BLACKBELT_I, FOCUS_BAND;
 	db GENDER_F25 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
