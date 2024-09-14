a1a85973175385ee6d544de7d782257451fdf7e2
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 02:23:57 +0100

    Update snorlax.asm

--- data/pokemon/base_stats/snorlax.asm
+++ data/pokemon/base_stats/snorlax.asm
@@ -7,7 +7,8 @@
 	db NORMAL, NORMAL ; type
 	db 25 ; catch rate
 	db 154 ; base exp
-	db LEFTOVERS, LEFTOVERS ; items
+	; db LEFTOVERS, LEFTOVERS ; items
+	db LEFTOVERS, RARE_CANDY;
 	db GENDER_F12_5 ; gender ratio
 	db 100 ; unknown 1
 	db 10 ; step cycles to hatch
