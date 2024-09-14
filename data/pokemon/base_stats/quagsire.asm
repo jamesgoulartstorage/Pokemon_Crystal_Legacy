bdfcd4e64be005ff52e1a5d7a5fad73b8a80e292
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 02:09:48 +0100

    Update quagsire.asm

--- data/pokemon/base_stats/quagsire.asm
+++ data/pokemon/base_stats/quagsire.asm
@@ -7,7 +7,8 @@
 	db WATER, GROUND ; type
 	db 90 ; catch rate
 	db 137 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db YLW_APRICORN, WATER_STONE;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
