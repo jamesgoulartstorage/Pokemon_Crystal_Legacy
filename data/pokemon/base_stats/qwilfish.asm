cf9085df278260584d76fa5917fa3f34fc7eecf9
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 02:10:31 +0100

    Update qwilfish.asm

--- data/pokemon/base_stats/qwilfish.asm
+++ data/pokemon/base_stats/qwilfish.asm
@@ -6,7 +6,8 @@
 	db WATER, DARK; POISON ; type
 	db 45 ; catch rate
 	db 100 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db FRESH_WATER, POISON_BARB;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
