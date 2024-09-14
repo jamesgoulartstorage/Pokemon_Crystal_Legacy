52298ca9511d96d7f02ad73144fd0427d3833135
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 02:35:04 +0100

    Update ursaring.asm

--- data/pokemon/base_stats/ursaring.asm
+++ data/pokemon/base_stats/ursaring.asm
@@ -7,7 +7,8 @@
 	db NORMAL, DARK; NORMAL ; type
 	db 60 ; catch rate
 	db 189 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db MINT_BERRY, MOON_STONE;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
