c139bdbc00aa713e415580a403ee83f15815e0af
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 02:03:52 +0100

    Update pineco.asm

--- data/pokemon/base_stats/pineco.asm
+++ data/pokemon/base_stats/pineco.asm
@@ -6,7 +6,8 @@
 	db BUG, GRASS; BUG ; type
 	db 190 ; catch rate
 	db 60 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db GRN_APRICORN, MIRACLE_SEED;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
