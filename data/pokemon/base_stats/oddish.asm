dc88613886964e2ad51a861f9b348c32b74790d6
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 01:59:25 +0100

    Update oddish.asm

--- data/pokemon/base_stats/oddish.asm
+++ data/pokemon/base_stats/oddish.asm
@@ -6,7 +6,8 @@
 	db GRASS, POISON ; type
 	db 255 ; catch rate
 	db 78 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db SILVER_LEAF, GOLD_BERRY;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
