9bd4116877d64c6ff7be9ff7caf21d584677d3fb
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 01:50:42 +0100

    Update marowak.asm

--- data/pokemon/base_stats/marowak.asm
+++ data/pokemon/base_stats/marowak.asm
@@ -7,7 +7,8 @@
 	db GROUND, DARK; GROUND ; type
 	db 75 ; catch rate
 	db 124 ; base exp
-	db NO_ITEM, THICK_CLUB ; items
+	; db NO_ITEM, THICK_CLUB ; items
+	db THICK_CLUB, BERSERK_GENE;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
