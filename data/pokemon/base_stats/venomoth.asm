f9ea7dcc3618d8a5dc69c1ad914896185c5417c5
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 02:35:37 +0100

    Update venomoth.asm

--- data/pokemon/base_stats/venomoth.asm
+++ data/pokemon/base_stats/venomoth.asm
@@ -7,7 +7,8 @@
 	db BUG, POISON ; type
 	db 75 ; catch rate
 	db 138 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db SILVERPOWDER, BRIGHTPOWDER;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
