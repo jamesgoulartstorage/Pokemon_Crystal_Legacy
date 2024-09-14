1b13f882a6eed63bb27dbf69a798ecd9a5355442
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 02:04:14 +0100

    Update pinsir.asm

--- data/pokemon/base_stats/pinsir.asm
+++ data/pokemon/base_stats/pinsir.asm
@@ -7,7 +7,8 @@
 	db BUG, FIGHTING; BUG ; type
 	db 45 ; catch rate
 	db 200 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db GRN_APRICORN, SILVERPOWDER;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 6 ; step cycles to hatch
