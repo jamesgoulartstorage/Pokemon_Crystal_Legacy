3d4c2d12a2aa7716947ffd6d92fd402fdbb077e5
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 02:12:58 +0100

    Update remoraid.asm

--- data/pokemon/base_stats/remoraid.asm
+++ data/pokemon/base_stats/remoraid.asm
@@ -7,7 +7,8 @@
 	db WATER, WATER ; type
 	db 190 ; catch rate
 	db 78 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db FRESH_WATER, FRESH_WATER;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
