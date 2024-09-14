8083ee717cc1361455f3ca48c1d18fb934e86dc4
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 02:42:51 +0100

    Update zubat.asm

--- data/pokemon/base_stats/zubat.asm
+++ data/pokemon/base_stats/zubat.asm
@@ -6,7 +6,8 @@
 	db POISON, FLYING ; type
 	db 255 ; catch rate
 	db 54 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db PSNCUREBERRY, RED_APRICORN;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 4 ; step cycles to hatch
