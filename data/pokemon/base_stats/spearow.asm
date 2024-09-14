2e8c1f1d19af07ee9055559c89eb9364e95fab59
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 02:24:44 +0100

    Update spearow.asm

--- data/pokemon/base_stats/spearow.asm
+++ data/pokemon/base_stats/spearow.asm
@@ -6,7 +6,8 @@
 	db NORMAL, FLYING ; type
 	db 255 ; catch rate
 	db 58 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db YLW_APRICORN, NUGGET;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 4 ; step cycles to hatch
