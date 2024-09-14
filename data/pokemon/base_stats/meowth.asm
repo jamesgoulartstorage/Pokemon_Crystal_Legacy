e89d689e435700930bc24ac355918bc27ad6d540
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 01:51:21 +0100

    Update meowth.asm

--- data/pokemon/base_stats/meowth.asm
+++ data/pokemon/base_stats/meowth.asm
@@ -6,7 +6,8 @@
 	db NORMAL, NORMAL ; type
 	db 255 ; catch rate
 	db 69 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db BLACKGLASSES, AMULET_COIN;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
