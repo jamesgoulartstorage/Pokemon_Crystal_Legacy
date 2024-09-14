0d22d50e84f2efaf33090c4482ebba5cde9a05fd
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 02:32:41 +0100

    Update typhlosion.asm

--- data/pokemon/base_stats/typhlosion.asm
+++ data/pokemon/base_stats/typhlosion.asm
@@ -6,7 +6,8 @@
 	db FIRE, GROUND; FIRE ; type
 	db 45 ; catch rate
 	db 209 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db BURNT_BERRY, CHARCOAL;
 	db GENDER_F12_5 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
