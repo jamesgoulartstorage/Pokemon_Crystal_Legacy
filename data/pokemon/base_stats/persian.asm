d3f5824e1eec2c2648ce14bf235cf5a76d614903
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 02:00:55 +0100

    Update persian.asm

--- data/pokemon/base_stats/persian.asm
+++ data/pokemon/base_stats/persian.asm
@@ -7,7 +7,8 @@
 	db NORMAL, DARK; NORMAL ; type
 	db 90 ; catch rate
 	db 148 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db QUICK_CLAW, QUICK_CLAW;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
