bd54200f1a4ffcca7ccd285244c6dafb3f95e36a
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 02:43:27 +0100

    Update squirtle.asm

--- data/pokemon/base_stats/squirtle.asm
+++ data/pokemon/base_stats/squirtle.asm
@@ -7,7 +7,8 @@
 	db 45 ; catch rate
 	db 66 ; base exp
 	; db NO_ITEM, NO_ITEM ; items
-	db FRESH_WATER, SOFT_SAND;
+	; db FRESH_WATER, SOFT_SAND;
+	db FRESH_WATER, STARDUST;
 	db GENDER_F12_5 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
