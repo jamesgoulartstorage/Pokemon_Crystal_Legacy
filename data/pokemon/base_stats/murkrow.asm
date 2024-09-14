7415de8f5af580c95ec41c93c3d6b49417fbd408
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 01:54:12 +0100

    Update murkrow.asm

--- data/pokemon/base_stats/murkrow.asm
+++ data/pokemon/base_stats/murkrow.asm
@@ -7,7 +7,8 @@
 	db DARK, FLYING ; type
 	db 30 ; catch rate
 	db 107 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db SHARP_BEAK, BLACKGLASSES;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
