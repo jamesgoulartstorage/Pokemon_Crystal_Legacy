b8040f43f1a9f52e4668facc2a0b55d28bc8d419
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 02:23:31 +0100

    Update sneasel.asm

--- data/pokemon/base_stats/sneasel.asm
+++ data/pokemon/base_stats/sneasel.asm
@@ -7,7 +7,8 @@
 	db DARK, ICE ; type
 	db 60 ; catch rate
 	db 132 ; base exp
-	db NO_ITEM, QUICK_CLAW ; items
+	; db NO_ITEM, QUICK_CLAW ; items
+	db BLACKGLASSES, QUICK_CLAW;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
