1eb1d1ebee7761e65fc52ee6fce145c761484bb0
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 02:04:39 +0100

    Update politoed.asm

--- data/pokemon/base_stats/politoed.asm
+++ data/pokemon/base_stats/politoed.asm
@@ -7,7 +7,8 @@
 	db WATER, GRASS; WATER ; type
 	db 45 ; catch rate
 	db 185 ; base exp
-	db NO_ITEM, KINGS_ROCK ; items
+	; db NO_ITEM, KINGS_ROCK ; items
+	db MYSTIC_WATER, KINGS_ROCK;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
