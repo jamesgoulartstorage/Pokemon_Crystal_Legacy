ac5034d95495233510f0b32d83209f183890d377
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 01:48:40 +0100

    Update mankey.asm

--- data/pokemon/base_stats/mankey.asm
+++ data/pokemon/base_stats/mankey.asm
@@ -7,7 +7,8 @@
 	db FIGHTING, NORMAL; FIGHTING ; type
 	db 190 ; catch rate
 	db 74 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db GOLD_BERRY, NUGGET;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
