f4951b3b2d069e19b88be3188de631ba406988f6
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 02:00:38 +0100

    Update parasect.asm

--- data/pokemon/base_stats/parasect.asm
+++ data/pokemon/base_stats/parasect.asm
@@ -7,7 +7,8 @@
 	db BUG, GHOST; GRASS ; type
 	db 75 ; catch rate
 	db 128 ; base exp
-	db TINYMUSHROOM, BIG_MUSHROOM ; items
+	; db TINYMUSHROOM, BIG_MUSHROOM ; items
+	db BIG_MUSHROOM, BIG_MUSHROOM;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
