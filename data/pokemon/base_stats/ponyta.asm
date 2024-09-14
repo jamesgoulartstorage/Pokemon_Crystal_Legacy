7b83ca46e67b0664ee89a4f4339a93d63b7b5848
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 02:07:24 +0100

    Update ponyta.asm

--- data/pokemon/base_stats/ponyta.asm
+++ data/pokemon/base_stats/ponyta.asm
@@ -6,7 +6,8 @@
 	db NORMAL, FIRE;, FIRE ; type
 	db 190 ; catch rate
 	db 152 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db PINK_BOW, CHARCOAL;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
