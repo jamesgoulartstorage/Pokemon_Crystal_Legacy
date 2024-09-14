8fea5fc129e9a05ddd7f41d52915ac2df4a5ef8c
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 02:41:13 +0100

    Update wobbuffet.asm

--- data/pokemon/base_stats/wobbuffet.asm
+++ data/pokemon/base_stats/wobbuffet.asm
@@ -7,7 +7,8 @@
 	db PSYCHIC_TYPE, DARK; PSYCHIC_TYPE ; type
 	db 45 ; catch rate
 	db 177 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db MINT_BERRY, BLACKGLASSES;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
