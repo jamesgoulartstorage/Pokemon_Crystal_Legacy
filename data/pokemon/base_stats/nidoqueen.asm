55782c6bae8e3cc773c2165ce3ef6fa5409c00b1
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 01:55:38 +0100

    Update nidoqueen.asm

--- data/pokemon/base_stats/nidoqueen.asm
+++ data/pokemon/base_stats/nidoqueen.asm
@@ -6,7 +6,8 @@
 	db POISON, GROUND ; type
 	db 45 ; catch rate
 	db 194 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db PNK_APRICORN, YLW_APRICORN;
 	db GENDER_F100 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
