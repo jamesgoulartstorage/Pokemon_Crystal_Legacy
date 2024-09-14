89b26e12f96f51426d157fd44641baab4a88482e
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 01:40:18 +0100

    Update kangaskhan.asm

--- data/pokemon/base_stats/kangaskhan.asm
+++ data/pokemon/base_stats/kangaskhan.asm
@@ -6,7 +6,8 @@
 	db NORMAL, GROUND; NORMAL ; type
 	db 45 ; catch rate
 	db 175 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db BLK_APRICORN, MIRACLEBERRY;
 	db GENDER_F100 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
