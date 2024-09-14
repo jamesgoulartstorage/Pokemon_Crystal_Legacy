5a7b5f9556851b6f91a5fe6c45300ad6fd560c30
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 02:02:56 +0100

    Update pidgey.asm

--- data/pokemon/base_stats/pidgey.asm
+++ data/pokemon/base_stats/pidgey.asm
@@ -7,7 +7,8 @@
 	db NORMAL, FLYING ; type
 	db 255 ; catch rate
 	db 55 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db BERRY, GOLD_BERRY;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 4 ; step cycles to hatch
