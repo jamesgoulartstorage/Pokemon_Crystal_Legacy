7d910104b027ce3a6893dd4fcf0d9ce20cb4e7d2
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 02:28:36 +0100

    Update swinub.asm

--- data/pokemon/base_stats/swinub.asm
+++ data/pokemon/base_stats/swinub.asm
@@ -6,7 +6,8 @@
 	db ICE, GROUND ; type
 	db 225 ; catch rate
 	db 78 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db ICE_BERRY, SOFT_SAND;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
