cd7afc36be02360e06a8b0425737ca337bb758a1
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 02:22:22 +0100

    Update smeargle.asm

--- data/pokemon/base_stats/smeargle.asm
+++ data/pokemon/base_stats/smeargle.asm
@@ -7,7 +7,8 @@
 	db NORMAL, GHOST; NORMAL ; type
 	db 45 ; catch rate
 	db 106 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db CLEANSE_TAG, SPELL_TAG;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
