dee159593d84e1a6f62db915dd3e2d70ed8222c4
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 02:07:06 +0100

    Update poliwrath.asm

--- data/pokemon/base_stats/poliwrath.asm
+++ data/pokemon/base_stats/poliwrath.asm
@@ -7,7 +7,8 @@
 	db WATER, FIGHTING ; type
 	db 45 ; catch rate
 	db 185 ; base exp
-	db NO_ITEM, KINGS_ROCK ; items
+	; db NO_ITEM, KINGS_ROCK ; items
+	db LUCKY_PUNCH, FOCUS_BAND;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
