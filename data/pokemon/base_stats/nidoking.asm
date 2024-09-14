cb39282cc485f1f1181b6330ff3e7d8c959ea29d
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 01:54:52 +0100

    Update nidoking.asm

--- data/pokemon/base_stats/nidoking.asm
+++ data/pokemon/base_stats/nidoking.asm
@@ -6,7 +6,8 @@
 	db POISON, GROUND ; type
 	db 45 ; catch rate
 	db 195 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db PNK_APRICORN, KINGS_ROCK;
 	db GENDER_F0 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
