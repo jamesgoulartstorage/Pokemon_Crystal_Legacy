24e22c0145190bd72baf0db78c56742ed72207f8
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 02:29:42 +0100

    Update tentacool.asm

--- data/pokemon/base_stats/tentacool.asm
+++ data/pokemon/base_stats/tentacool.asm
@@ -6,7 +6,8 @@
 	db WATER, POISON ; type
 	db 190 ; catch rate
 	db 105 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db SMOKE_BALL, POISON_BARB;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
