f741902af87dde14a1d8b434f833fe0e44e3d006
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 01:46:47 +0100

    Update magcargo.asm

--- data/pokemon/base_stats/magcargo.asm
+++ data/pokemon/base_stats/magcargo.asm
@@ -7,7 +7,8 @@
 	db FIRE, ROCK ; type
 	db 75 ; catch rate
 	db 154 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db FIRE_STONE, SUN_STONE;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
