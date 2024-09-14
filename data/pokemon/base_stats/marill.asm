2e65005e45be5d70b2e9eb9423b91ca6cc96da86
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 01:50:17 +0100

    Update marill.asm

--- data/pokemon/base_stats/marill.asm
+++ data/pokemon/base_stats/marill.asm
@@ -7,7 +7,8 @@
 	db WATER, NORMAL; WATER ; type
 	db 190 ; catch rate
 	db 58 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db FRESH_WATER, POLKADOT_BOW;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
