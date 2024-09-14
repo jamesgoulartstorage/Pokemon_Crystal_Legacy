7cfbd2fa4f5cc0e493bb734e0ffc8e46dad68a43
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 02:03:33 +0100

    Update piloswine.asm

--- data/pokemon/base_stats/piloswine.asm
+++ data/pokemon/base_stats/piloswine.asm
@@ -6,7 +6,8 @@
 	db ICE, GROUND ; type
 	db 75 ; catch rate
 	db 160 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db BLK_APRICORN, NEVERMELTICE;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
