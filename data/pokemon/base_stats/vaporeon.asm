766d571ed1fe8b8b2e91037d1cf623a3af06a7b5
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 02:35:20 +0100

    Update vaporeon.asm

--- data/pokemon/base_stats/vaporeon.asm
+++ data/pokemon/base_stats/vaporeon.asm
@@ -6,7 +6,8 @@
 	db WATER, WATER ; type
 	db 45 ; catch rate
 	db 196 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db BLU_APRICORN, MYSTIC_WATER;
 	db GENDER_F12_5 ; gender ratio
 	db 100 ; unknown 1
 	db 9 ; step cycles to hatch
