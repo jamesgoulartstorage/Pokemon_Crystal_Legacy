1b825044a783b56eb01b86390de342ac32098a42
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 02:27:20 +0100

    Update sudowoodo.asm

--- data/pokemon/base_stats/sudowoodo.asm
+++ data/pokemon/base_stats/sudowoodo.asm
@@ -7,7 +7,8 @@
 	db ROCK, GRASS; ROCK ; type
 	db 65 ; catch rate
 	db 135 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db EVERSTONE, LEAF_STONE;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
