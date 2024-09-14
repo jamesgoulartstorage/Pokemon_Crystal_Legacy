ed53d1504a86f28ac20755fee4727aadc9bbaf18
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 02:16:33 +0100

    Update scyther.asm

--- data/pokemon/base_stats/scyther.asm
+++ data/pokemon/base_stats/scyther.asm
@@ -7,7 +7,8 @@
 	db BUG, FLYING ; type
 	db 45 ; catch rate
 	db 187 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db SILVERPOWDER, METAL_COAT;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 6 ; step cycles to hatch
