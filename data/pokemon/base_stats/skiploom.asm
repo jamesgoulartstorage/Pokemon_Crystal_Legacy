41cf26616260d490346889a48b76c6334120f62a
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 02:18:55 +0100

    Update skiploom.asm

--- data/pokemon/base_stats/skiploom.asm
+++ data/pokemon/base_stats/skiploom.asm
@@ -6,7 +6,8 @@
 	db GRASS, FLYING ; type
 	db 120 ; catch rate
 	db 136 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db GRN_APRICORN, MIRACLE_SEED;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
