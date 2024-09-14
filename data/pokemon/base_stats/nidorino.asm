cc73a448df1a44d21ddb72151e72d161dfbb3ee7
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 01:58:00 +0100

    Update nidorino.asm

--- data/pokemon/base_stats/nidorino.asm
+++ data/pokemon/base_stats/nidorino.asm
@@ -6,7 +6,8 @@
 	db POISON, POISON ; type
 	db 120 ; catch rate
 	db 118 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db PNK_APRICORN, POISON_BARB;
 	db GENDER_F0 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
