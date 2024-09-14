225a2147565a9719767f4e83841d03df79d38309
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 02:37:49 +0100

    Update vileplume.asm

--- data/pokemon/base_stats/vileplume.asm
+++ data/pokemon/base_stats/vileplume.asm
@@ -6,7 +6,8 @@
 	db GRASS, POISON ; type
 	db 45 ; catch rate
 	db 184 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db PSNCUREBERRY, GOLD_BERRY;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
