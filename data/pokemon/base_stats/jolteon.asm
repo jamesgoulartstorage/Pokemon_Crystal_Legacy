635c62235806ef652af70e41537b3b4670d0ecad
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 01:37:11 +0100

    Update jolteon.asm

--- data/pokemon/base_stats/jolteon.asm
+++ data/pokemon/base_stats/jolteon.asm
@@ -6,7 +6,8 @@
 	db ELECTRIC, ELECTRIC ; type
 	db 45 ; catch rate
 	db 197 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db YLW_APRICORN, MAGNET;
 	db GENDER_F12_5 ; gender ratio
 	db 100 ; unknown 1
 	db 9 ; step cycles to hatch
