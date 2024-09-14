83f11e0f85be92b8ace3ff4caade10736c6751a4
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 02:25:03 +0100

    Update spinarak.asm

--- data/pokemon/base_stats/spinarak.asm
+++ data/pokemon/base_stats/spinarak.asm
@@ -7,7 +7,8 @@
 	db BUG, POISON ; type
 	db 255 ; catch rate
 	db 54 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db PSNCUREBERRY, POISON_BARB;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 4 ; step cycles to hatch
