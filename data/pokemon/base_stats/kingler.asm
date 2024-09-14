a5b4772310b93ad43b1b8894043e2f77d3bcab3d
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 01:41:21 +0100

    Update kingler.asm

--- data/pokemon/base_stats/kingler.asm
+++ data/pokemon/base_stats/kingler.asm
@@ -7,7 +7,8 @@
 	db WATER, STEEL; WATER ; type
 	db 60 ; catch rate
 	db 206 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db ICE_BERRY, WATER_STONE;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
