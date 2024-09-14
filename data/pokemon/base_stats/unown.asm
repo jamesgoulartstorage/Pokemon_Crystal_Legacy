0bffa1eef84f600d77bc0027208c63d0e01b7fc7
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 02:34:45 +0100

    Update unown.asm

--- data/pokemon/base_stats/unown.asm
+++ data/pokemon/base_stats/unown.asm
@@ -7,7 +7,8 @@
 	db PSYCHIC_TYPE, GHOST; PSYCHIC_TYPE ; type
 	db 225 ; catch rate
 	db 61 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db STARDUST, BRIGHTPOWDER;
 	db GENDER_UNKNOWN ; gender ratio
 	db 100 ; unknown 1
 	db 10 ; step cycles to hatch
