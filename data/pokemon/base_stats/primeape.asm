b23954ef4a2a13379d3901676659610ad3391037
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 02:08:46 +0100

    Update primeape.asm

--- data/pokemon/base_stats/primeape.asm
+++ data/pokemon/base_stats/primeape.asm
@@ -7,7 +7,8 @@
 	db FIGHTING, NORMAL; FIGHTING ; type
 	db 75 ; catch rate
 	db 149 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db LUCKY_PUNCH, BERSERK_GENE;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
