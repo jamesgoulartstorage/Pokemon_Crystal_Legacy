e68ea7062dde7e5d1ca459239e28c552928930fb
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 02:29:07 +0100

    Update tauros.asm

--- data/pokemon/base_stats/tauros.asm
+++ data/pokemon/base_stats/tauros.asm
@@ -7,7 +7,8 @@
 	db NORMAL, FIGHTING; NORMAL ; type
 	db 45 ; catch rate
 	db 211 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db BLK_APRICORN, POLKADOT_BOW;
 	db GENDER_F0 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
