e82567698f6ae4af8803638b29be3a687380d722
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 01:54:28 +0100

    Update natu.asm

--- data/pokemon/base_stats/natu.asm
+++ data/pokemon/base_stats/natu.asm
@@ -6,7 +6,8 @@
 	db PSYCHIC_TYPE, FLYING ; type
 	db 190 ; catch rate
 	db 73 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db MINT_BERRY, TWISTEDSPOON;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
