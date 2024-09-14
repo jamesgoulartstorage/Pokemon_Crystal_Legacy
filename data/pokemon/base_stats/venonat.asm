5503b8df7a9fc81f2bcdac410a44bbf80a4afc10
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 02:35:55 +0100

    Update venonat.asm

--- data/pokemon/base_stats/venonat.asm
+++ data/pokemon/base_stats/venonat.asm
@@ -6,7 +6,8 @@
 	db BUG, POISON ; type
 	db 190 ; catch rate
 	db 75 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db SCOPE_LENS, SILVERPOWDER;
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
