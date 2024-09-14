73bdc62e9b76f6b24c5ae3740cad83520227f7ca
jamesgoulartstorage <james.goulart.storage@gmail.com>
2024-09-14 01:45:14 +0100

    Update machamp.asm

--- data/pokemon/base_stats/machamp.asm
+++ data/pokemon/base_stats/machamp.asm
@@ -7,7 +7,8 @@
 	db FIGHTING, DARK; FIGHTING ; type
 	db 45 ; catch rate
 	db 193 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	; db NO_ITEM, NO_ITEM ; items
+	db BLACKBELT_I, KINGS_ROCK;
 	db GENDER_F25 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
